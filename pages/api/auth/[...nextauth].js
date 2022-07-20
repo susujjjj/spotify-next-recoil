744b82bef273f00f548d842cdfaa15e8ead63392
sbj2 <sbj@ryencatchers.com>
2022-07-21 02:06:11 +0900

    spotify nextjs 연동

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,30 @@
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
+import spotifyApi, { LOGIN_URL } from '../../../lib/spotify';
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log('REFRESHED TOKEN IS', refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token, //accesstoekn is expired
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, //= 1hour as 3600 returns from spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken, //refresh Token이 존재한다면 계속해서 사용해라 .
+    }; //token refresh할때도 이 리턴값을 반환
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +36,43 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // initial sign in //로그인 하면 이 토큰을 가져옴
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: Date.now() + account.expires_at * 1000, //we are handling expiry times in
+          // refreshToken: account.refresh_token,
+          // user,
+        };
+      }
+
+      //Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('EXISTING ACCESS TOKEN IS VALID');
+        return token;
+      }
+
+      //Access token has expired, so we need to refresh it...
+      console.log('ACCESS TOKEN HAS EXPIRED, REFRESHING...');
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      console.log(session, ' session??');
+      session.user.accessToken = token.accessToken; //this is the part which the user can see. but the token, the user can't see
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
 });
